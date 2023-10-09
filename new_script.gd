04f3f2b651c0d3847449f1c69e85eb359a15ca55
Daniil Ermolaev <blcklptn@icloud.com>
2023-10-10 02:34:02 +0500

    Zaebumba 2.0

--- new_script.gd
+++ new_script.gd
@@ -1,6 +1,7 @@
 extends Sprite3D
 
 const SPEED = 3
+const HSPEED = SPEED / 2
 
 func _process(delta):
 	var dir = Vector2i()
@@ -13,10 +14,10 @@ func _process(delta):
 		speed_normalizer(dir, delta, SPEED)
 	if Input.is_action_pressed("ui_left"):
 		dir.x = -1
-		speed_normalizer(dir, delta, SPEED / 2)
+		speed_normalizer(dir, delta, HSPEED)
 	if Input.is_action_pressed("ui_right"):
 		dir.x = 1
-		speed_normalizer(dir, delta, SPEED / 2)
+		speed_normalizer(dir, delta, HSPEED)
 		
 
 func speed_normalizer(dir, delta, speed):
